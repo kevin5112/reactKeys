7b620b5a864d6f9bfd5afae76e0edac78386a80f
Kevin Chen <kevin5112@yahoo.com>
2024-02-12 15:37:10 -0800

    add logic with uuid for keys

---
+++ src/TodoList.jsx
@@ -0,0 +1,17 @@
+import { v4 as uuid } from 'uuid';
+
+const todos = [
+  { task: 'mow the yard', id: uuid() },
+  { task: 'Work on Odin Projects', id: uuid() },
+  { task: 'feed the cat', id: uuid() },
+];
+
+export default function TodoList() {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.task}</li>
+      ))}
+    </ul>
+  );
+}
